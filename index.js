87c506e68a6386f1dd187c32e425519f377facf1
CarlosFerMed <harmonium_skies@hotmail.com>
2023-11-09 14:51:53 +0100

    antes de ejecutar el comando git rm -r --cached

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const app = express();
+const app = require("express").express();
+
 const router = require("./app/router");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors());                                    // permite la entrada desde otro dominio diferente al original.
 app.use(express.text());                            // para que Express procese texto si la request llega en formato texto.
 app.use(express.json());                            // para que Express procese json si la request llega en formato json.
-app.use(express.urlencoded({extended: false}));     // form-encode
+// app.use(express.urlencoded({extended: false}));     // form-encode
 
 app.use("/", router);
 
